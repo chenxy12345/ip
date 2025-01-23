1f84339dc5bebb5745d87b7a1172125531ecdc92
chenxy12345 <xiangyun04@gmail.com>
2025-01-23 23:18:21 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Elmacho.java
@@ -0,0 +1,11 @@
+public class Elmacho {
+    public static void main(String[] args) {
+        String name = "Elmacho";
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
