c03b6a50417773afe8848daa6810b2feb88e1943
chenxyy <xiangyun04@gmail.com>
2025-01-24 02:30:07 +0800

    Revert "Add, List"
    This reverts commit 137005fbf90d410ebc3e76dacac3cee2f4134ad7.

--- src/main/java/Elmacho.java
+++ src/main/java/Elmacho.java
@@ -9,34 +9,23 @@ public class Elmacho {
         System.out.println("What you want?");
         System.out.println("____________________________________________________________");
         Scanner scanner = new Scanner(System.in);
-        int n = 0;
-        String[] arrayA = new String[100];
         while (true) {
-            String instruction = scanner.nextLine();
+            String instruction = scanner.next();
             if (Objects.equals(instruction, "list")) {
                 System.out.println("____________________________________________________________");
-                for (int i = 0; i < n; i++) {
-                    System.out.println(i + 1 + ". " + arrayA[i]);
-                }
+                System.out.println("here. your list");
                 System.out.println("____________________________________________________________");
             }
-            else if (Objects.equals(instruction, "blah")) {
+            if (Objects.equals(instruction, "blah")) {
                 System.out.println("____________________________________________________________");
                 System.out.println("blahblahblahblahblah");
                 System.out.println("____________________________________________________________");
             }
-            else if (Objects.equals(instruction, "bye")) {
+            if (Objects.equals(instruction, "bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println("OkBye.");
                 System.out.println("____________________________________________________________");
                 break;
-            }
-            else {
-                arrayA[n] = instruction;
-                n = n + 1;
-                System.out.println("____________________________________________________________");
-                System.out.println("added: " + instruction);
-                System.out.println("____________________________________________________________");
             }
         }
     }
