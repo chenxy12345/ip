b0ba3b0d1e5773a248cd3ad61375f3fb2dfa59b5
chenxyy <xiangyun04@gmail.com>
2025-01-23 23:42:10 +0800

    Echo

--- src/main/java/Elmacho.java
+++ src/main/java/Elmacho.java
@@ -1,11 +1,32 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Elmacho {
     public static void main(String[] args) {
         String name = "Elmacho";
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm " + name);
-        System.out.println("What can I do for you?");
+        System.out.println("What. I'm " + name);
+        System.out.println("What you want?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String instruction = scanner.next();
+            if (Objects.equals(instruction, "list")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("here. your list");
+                System.out.println("____________________________________________________________");
+            }
+            if (Objects.equals(instruction, "blah")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("blahblahblahblahblah");
+                System.out.println("____________________________________________________________");
+            }
+            if (Objects.equals(instruction, "bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("OkBye.");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+        }
     }
 }
